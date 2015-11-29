d808fe3f201c37ce90849568c452034eb59d206f
lwj1396@163.com <lwj1396@163.com>
2015-11-29 22:30:54 +0800

    nlarget and sorted

--- 1_4.py
+++ 1_4.py
@@ -1,7 +1,12 @@
+# 当要查找的元素个数相对比较小的时候，函数 nlargest() 和 nsmallest() 是很合适的。 如果你仅仅想查找唯一的最小或最大(N=1)的元素的话，
+# 那么使用min()和max()函数会更快些。 类似的，如果N的大小和集合大小接近的时候，通常先排序这个集合然后再使用切片操作会更快点
+#( sorted(items)[:N] 或者是 sorted(items)[-N:] )
 import heapq
 
 if __name__ == '__main__':
     nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+    print(heapq.heapify(nums))
+    print(nums)
     print(heapq.nlargest(3, nums))
     print(heapq.nsmallest(3, nums))
 
